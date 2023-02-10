b34eceece09a6f2a03333f8ad8480a3a37683781
MarcosLabra <mlabra.dev@gmail.com>
2023-02-10 10:29:27 -0300

    1er desafio

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.background}>
+      <LinearGradient
+        style={styles.frame}
+        colors={['#FF00FF', '#FCEE21']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <View style={styles.container}>
+          <Text style={styles.text}>Hola Coder!</Text>
+        </View>
+      </LinearGradient>
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  frame: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 200,
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    width: 280,
+    height: 180,
+    borderRadius: 2,
+  },
+  text: {
+    color: '#fbfbfb',
+    fontSize: 20,
+  }
 });
